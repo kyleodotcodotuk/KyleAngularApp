316984b1e101e86b5ef048af2cb77d499e9c1899
Kyle O'Connor <kyle1359@gmail.com>
2023-11-23 17:50:26 +0000

    Assets and renaming selector

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
+  selector: 'website',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './main.html',
